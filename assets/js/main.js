668cd9de4e1570a10b3feb9d467b4966c71737c3
aluno3 <rnatanyell@gmail.com>
2024-02-29 15:44:51 -0300

    portifolio

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
+
+/*==================== ABOUT TABS ====================*/
+
+/*=============== CONTACT FORM =============== */
