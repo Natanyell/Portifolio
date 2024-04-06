bcdee2ab529472a0b95e1aeeaf1c702d5c4798c6
Natanyell <rnatanyell@gmail.com>
2024-04-06 15:54:30 -0300

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,26 +18,26 @@
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]');
 
-    tabs.forEach((tab) => {
-        tab.addEventListener('click', () => {
-            const target = document.querySelector(tab.dataset.target);
-            
-            tabContents.forEach((tabContents) => {
-                tabContents.classList.remove('tab__active');
-            });
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
 
-            target.classList.add('tab__active');
-
-            tabs.forEach((tab) => {
-                tab.classList.remove('tab__active')
-            });
-
-            tab.classList.add('tab__active')
+        tabContents.forEach((tabContents) => {
+            tabContents.classList.remove('tab__active');
         });
+
+        target.classList.add('tab__active');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active')
+        });
+
+        tab.classList.add('tab__active')
     });
+});
 
 /*=============== CONTACT FORM =============== */
-const contactForm =  document.getElementById("contact-form"),
+const contactForm = document.getElementById("contact-form"),
     contactName = document.getElementById('contact-name'),
     contactEmail = document.getElementById('contact-email'),
     contactSubject = document.getElementById('contact-subject'),
@@ -48,11 +48,30 @@ const sendEmail = (e) => {
     e.preventDefault();
 
     //verifica se o campo possui valor
-    if(contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
+    if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
         // show message 
         errorMessage.textContent = 'Escreva todos os campos de entrada'
     } else {
+        //serviceID - templateID - #form - publickey
+        emailjs.sendForm('service_modlqtk', 'template_rzyolyt', '#contact-form', 'O7qSaAdpgvxhozSVn').then(() => {
+            errorMessage.classList.add('color-first');
+            errorMessage.textContent = 'Mensagem enviada ✔️';
 
+            //remove message after 5 seconds
+            setTimeout(() => {
+                errorMessage.textContent = '';
+            }, 5000);
+        },
+            (error) => {
+                alert('Ops! ALGO DEU ERRADO...', error);
+            }
+        );
+
+        // clear input  fields
+        contactName.value = '';
+        contactEmail.value = '';
+        contactSubject.value = '';
+        contactMessage.value = '';
     }
 };
 
