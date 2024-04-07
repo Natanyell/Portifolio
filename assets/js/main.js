0fe8415a72dc9d2230d1b62d2b7e408cbf44fd66
Natanyell <rnatanyell@gmail.com>
2024-04-07 16:57:53 -0300

    Finalização Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,52 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById("header");
-    if(this.scrollY >= 80) header.classList.add('scroll-header');
-    else header.classList.remove( 'scroll-header' );
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
 }
 
-window.addEventListener( "scroll", scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollUP(){
+function scrollUP() {
     const scrollUp = document.getElementById("scroll-up");
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-    else scrollUp.classList.remove( 'show-scroll' );
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
 }
 
-window.addEventListener( "scroll", scrollUP);
+window.addEventListener("scroll", scrollUP);
 
 /*==================== ABOUT TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
